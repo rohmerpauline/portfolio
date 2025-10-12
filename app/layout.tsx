fc97b970f27a2a289faa62f176fa9587a9226508
rohmerpauline <rohmer.pauline@outlook.com>
2025-10-12 16:18:06 +0200

    feat: use all components and assemble page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,18 @@
 import type { Metadata } from 'next';
 import { Fira_Code, Poppins } from 'next/font/google';
 
+import { Footer } from './components/Footer/Footer';
+import { Header } from './components/Header/Header';
 import './globals.css';
 
 const poppins = Poppins({
-  weight: '400',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
   subsets: ['latin'],
   variable: '--font-poppins',
 });
 
 const firaCode = Fira_Code({
+  weight: ['300', '400', '500', '600', '700'],
   subsets: ['latin'],
   variable: '--fira-code',
 });
@@ -27,9 +30,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${poppins.variable} ${firaCode.variable} $ antialiased`}
+        className={`${poppins.variable} ${firaCode.variable} $ antialiased relative`}
       >
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
+import { About } from './components/About/About';
+import { HeroContainer } from './components/HeroContainer/HeroContainer';
+import { MyExperience } from './components/MyExperience/MyExperience';
+import { MyWork } from './components/MyWork/MyWork';
 
 export default function Home() {
   return (
-    <div className='font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20'>
-      NextJs Template
+    <div className="">
+      <HeroContainer />
+      <MyWork />
+      <MyExperience />
+      <About />
     </div>
   );
 }
